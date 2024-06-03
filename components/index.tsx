c4b375b32d4cc6938dff3c5680414072bc265625
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-06-03 11:23:02 +0300

    feat: add Navbar component

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,6 @@
+import Footer from "./Footer";
 import Hero from "./Hero";
+import Navbar from "./Navbar";
 import CustomButton from "./common/CustomButton";
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, Navbar, Footer };
