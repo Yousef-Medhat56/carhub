5b77d486eea509f4e61cfe0e7d4dc4fe3610dd5b
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-06-04 11:03:46 +0300

    feat: add CarCard component

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,19 @@
+import CarCard from "./CarCard";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchManufacturer from "./SearchManufacturer";
 import CustomButton from "./common/CustomButton";
+import CustomFilter from "./common/CustomFilter";
+import SearchBar from "./common/SearchBar";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+};
--- types/index.ts
+++ types/index.ts
@@ -3,10 +3,26 @@ export interface CustomButtonProps {
   type?: "button" | "submit" | "reset" | undefined;
   disabled?: boolean;
   containerStyles?: string;
+  rightIcon?: string;
   handleClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export interface SearchManufacturerProps{
-  manufacturer:string|null
-  setManufacturer:React.Dispatch<React.SetStateAction<string|null>>
+export interface SearchManufacturerProps {
+  manufacturer: string | null;
+  setManufacturer: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
