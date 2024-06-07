bee9a6986d3022021ff3b2642a6e1a10c5114f54
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-06-07 18:46:13 +0300

    feat(CustomButton): add rightIcon prop to the component

--- components/common/CustomButton.tsx
+++ components/common/CustomButton.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 import React from "react";
 import { CustomButtonProps } from "@/types";
-
+import Image from "next/image";
+Image;
 
 const CustomButton = ({
   title,
   type = "button",
   disabled = false,
   containerStyles,
+  rightIcon,
   handleClick,
 }: CustomButtonProps) => {
   return (
@@ -17,9 +19,17 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className="flex-1">
-        {title}
-      </span>
+      <span className="flex-1">{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
